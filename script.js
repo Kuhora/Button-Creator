36be2328323f4c7668020ce92d0a6030eb78259e
Matheus Fragasse <kuhora@gmail.com>
2023-05-18 14:40:32 -0300

    Final Project

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ const cssText = document.querySelector('.css');
 const btn = document.querySelector('.btn');
 controles.addEventListener('change', handleChange);
 
-
 const handleStyle = {
   element: btn,
-  backgroundColor(value){
+  backgroundColor(value) {
     this.element.style.backgroundColor = value;
   },
   height(value) {
@@ -22,7 +21,7 @@ const handleStyle = {
     this.element.style.color = value;
   },
   border(value) {
-    this.element.style.border= value;
+    this.element.style.border = value;
   },
   borderRadius(value) {
     this.element.style.borderRadius = value + 'px';
@@ -32,15 +31,33 @@ const handleStyle = {
   },
   fontSize(value) {
     this.element.style.fontSize = value + 'rem';
-  }
+  },
 }
+
 function handleChange(event) {
   const name = event.target.name;
   const value = event.target.value;
 
   handleStyle[name](value);
+  saveValues(name, value);
   showCss();
 }
+
+function saveValues(name, value) {
+  localStorage[name] = value;
+}
+
+function setValues() {
+  const properties = Object.keys(localStorage);
+  properties.forEach(propertie => {
+    handleStyle[propertie](localStorage[propertie]);
+    controles.elements[propertie].value = localStorage[propertie];
+  });
+  showCss();
+}
+
+setValues();
+
 function showCss() {
-  cssText.innerHTML =  '<span>' + btn.style.cssText.split ('; ').join(';</span><span>');
+  cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>');
 }
